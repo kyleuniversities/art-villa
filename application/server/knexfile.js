a7fbbe3849ec19b8925c1a34c119f66d544fbaf3
kyleuniversities <kyleuniversities@gmail.com>
2023-10-23 01:38:17 -0700

    added port optionality to knex connection

--- application/server/knexfile.js
+++ application/server/knexfile.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 // Connection data
 const DATABASE_HOST = process.env['DATABASE_HOST'];
+const DATABASE_PORT = process.env['DATABASE_PORT'];
 const DATABASE_USERNAME = process.env['DATABASE_USERNAME'];
 const DATABASE_PASSWORD = process.env['DATABASE_PASSWORD'];
 
@@ -15,6 +16,7 @@ const makeSqlBuilder = (databaseKey) => {
     client: 'mysql2',
     connection: {
       host: DATABASE_HOST,
+      port: DATABASE_PORT,
       user: DATABASE_USERNAME,
       password: DATABASE_PASSWORD,
       database: process.env[databaseKey],
